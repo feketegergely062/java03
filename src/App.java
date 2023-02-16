6289b3dea6cc47ebf780dec7fc546fa2531d358d
Fekete Gergely <feketegergely062@gmail.com>
2023-02-16 15:18:14 +0100

    harom osztalyra botas

--- src/App.java
+++ src/App.java
@@ -1,30 +1,6 @@
-import java.util.Scanner;
-
 public class App{
-    public static double calcRadius(double side, double alpha){
-        double rad = alpha * Math.PI/180;
-        double radius = 1/2.*side *Math.sin(alpha);
-        return radius;
-
-    }
-
-    public static void feladat0312(){
-       System.out.println("0312 megoldás");
-       System.out.println("Rombuszba írható kör sugara");
-        //Bekérés
-       // A Scanner osztály példányosítása
-       Scanner scanner = new Scanner(System.in);
-       System.out.print("Oldal:");
-       double side = Double.parseDouble(scanner.nextLine());
-       System.out.print("Alfa szög:");
-       double alpha = Double.parseDouble(scanner.nextLine());
-       scanner.close();
-       double radius = calcRadius(side, alpha);
-       System.out.println("Sugar:"+radius);
-
-
-    }
     public static void main(String[] args) throws Exception {
-   feladat0312();
+   Tombsug tombsug = new Tombsug();
+   
     }
 }
